be417a7cfe21cdbffccafbc451b2454d8a6ad88e
DanaMichelleZ <dana.zambelli@davinci.edu.ar>
2024-10-14 16:36:00 -0300

    com

--- api-vocaloid/public/llamamos-de-api.js
+++ api-vocaloid/public/llamamos-de-api.js
@@ -1,25 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
     const vocaloidList = document.getElementById("vocaloid-list");
 
-    vocaloidList.innerHTML = "";
+    if (vocaloidList) {
+        vocaloidList.innerHTML = "";
+    }
 
     fetch("/vocaloids")
         .then(response => response.json())
         .then(data => {
+            if (vocaloidList) {
 
-            data.forEach(vocaloid => {
-                const vocaloidElement = document.createElement("li");
-                vocaloidElement.classList.add("vocaloid-item");
+                data.forEach(vocaloid => {
+                    const vocaloidElement = document.createElement("li");
+                    vocaloidElement.classList.add("vocaloid-item");
 
-                vocaloidElement.innerHTML = `
-                    <h2>${vocaloid.nombre}</h2>
-                    <img src="${vocaloid.imagenPerfil}" alt="${vocaloid.nombre} - Imagen de perfil" />
-                `;
-                vocaloidList.appendChild(vocaloidElement);
-            });
+                    vocaloidElement.innerHTML = `
+                        <h2>${vocaloid.nombre}</h2>
+                        <img src="${vocaloid.imagenPerfil}" alt="${vocaloid.nombre} - Imagen de perfil" />
+                    `;
+                    vocaloidList.appendChild(vocaloidElement);
+                });
+            }
         })
         .catch(error => {
             console.error("Error al obtener los vocaloids:", error);
-            vocaloidList.innerHTML = "<p>Error al cargar los vocaloids.</p>";
+            if (vocaloidList) {
+                vocaloidList.innerHTML = "<p>Error al cargar los vocaloids.</p>";
+            }
         });
 });
